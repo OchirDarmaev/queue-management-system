950d0ae0dc8e1ee62612e63f91ae8bab064e3a1b
Ochir Darmaev <ochirdarmaev@gmail.com>
2023-04-30 03:24:34 +0300

    add publish test message to topic

--- src/test/test.ts
+++ src/test/test.ts
@@ -27,13 +27,49 @@ export const handlerProtected = async (
     body: `Hello World ${sub} ${email}`,
   };
 };
+import {
+  IoTDataPlaneClient,
+  PublishCommand,
+} from "@aws-sdk/client-iot-data-plane"; // ES Modules import
 
 export const handlerPublic = async (event: APIGatewayProxyEvent, _context) => {
-  // log event to dynamodb table
-  console.log(JSON.stringify(event, null, 2));
-  console.log("Hello World");
-  return {
-    statusCode: 200,
-    body: "Hello World Public",
-  };
+  try {
+    const client = new IoTDataPlaneClient({});
+    const messageToSend = {
+      message: "Hello from Lambda",
+      timestamp: new Date().toISOString(),
+    };
+
+    const topicPrefix = process.env.TOPIC_PREFIX;
+    if (!topicPrefix) {
+      throw new Error("TOPIC_PREFIX is not defined");
+    }
+
+    const topicName = `${topicPrefix}/test-topic`;
+    console.log(`Publishing to topic: ${topicName}`);
+
+    const iotPublishCommand = new PublishCommand({
+      topic: topicName,
+      payload: Buffer.from(JSON.stringify(messageToSend)),
+    });
+
+    const response = await client.send(iotPublishCommand);
+
+    console.log(JSON.stringify(event, null, 2));
+    console.log("Hello World");
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        response,
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        err,
+      }),
+    };
+  }
 };
